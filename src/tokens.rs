33eb276a63c2fd6416f0fb1e5bf8bc41d94c6be7
Nika Layzell <nika@thelayzells.com>
2018-01-08 16:17:00 -0500

    Implement Hash and PartialEq for quote::Tokens

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,5 +1,6 @@
 use super::ToTokens;
 use std::fmt::{self, Debug, Display};
+use std::hash::{Hash, Hasher};
 
 use proc_macro;
 use proc_macro2::{TokenStream, TokenTree};
@@ -165,5 +166,99 @@ impl Debug for Tokens {
             .debug_tuple("Tokens")
             .field(&DebugAsDisplay(self))
             .finish()
+    }
+}
+
+fn tt_eq(a: &TokenTree, b: &TokenTree) -> bool {
+    use proc_macro2::{TokenNode, Delimiter, Spacing};
+
+    match (&a.kind, &b.kind) {
+        (&TokenNode::Group(d1, ref s1), &TokenNode::Group(d2, ref s2)) => {
+            match (d1, d2) {
+                (Delimiter::Parenthesis, Delimiter::Parenthesis)
+                | (Delimiter::Brace, Delimiter::Brace)
+                | (Delimiter::Bracket, Delimiter::Bracket)
+                | (Delimiter::None, Delimiter::None) => {}
+                _ => return false,
+            }
+
+            let s1 = s1.clone().into_iter();
+            let mut s2 = s2.clone().into_iter();
+
+            for item1 in s1 {
+                let item2 = match s2.next() {
+                    Some(item) => item,
+                    None => return false,
+                };
+                if !tt_eq(&item1, &item2) {
+                    return false;
+                }
+            }
+            s2.next().is_none()
+        }
+        (&TokenNode::Op(o1, k1), &TokenNode::Op(o2, k2)) => {
+            o1 == o2 && match (k1, k2) {
+                (Spacing::Alone, Spacing::Alone) | (Spacing::Joint, Spacing::Joint) => true,
+                _ => false,
+            }
+        }
+        (&TokenNode::Literal(ref l1), &TokenNode::Literal(ref l2)) => {
+            l1.to_string() == l2.to_string()
+        }
+        (&TokenNode::Term(ref s1), &TokenNode::Term(ref s2)) => s1.as_str() == s2.as_str(),
+        _ => false,
+    }
+}
+
+impl PartialEq for Tokens {
+    fn eq(&self, other: &Self) -> bool {
+        if self.tts.len() != other.tts.len() {
+            return false;
+        }
+
+        self.tts
+            .iter()
+            .zip(other.tts.iter())
+            .all(|(a, b)| tt_eq(a, b))
+    }
+}
+
+fn tt_hash<H: Hasher>(tt: &TokenTree, h: &mut H) {
+    use proc_macro2::{TokenNode, Delimiter, Spacing};
+
+    match tt.kind {
+        TokenNode::Group(delim, ref stream) => {
+            0u8.hash(h);
+            match delim {
+                Delimiter::Parenthesis => 0u8.hash(h),
+                Delimiter::Brace => 1u8.hash(h),
+                Delimiter::Bracket => 2u8.hash(h),
+                Delimiter::None => 3u8.hash(h),
+            }
+
+            for item in stream.clone() {
+                tt_hash(&item, h);
+            }
+            0xffu8.hash(h); // terminator w/ a variant we don't normally hash
+        }
+        TokenNode::Op(op, kind) => {
+            1u8.hash(h);
+            op.hash(h);
+            match kind {
+                Spacing::Alone => 0u8.hash(h),
+                Spacing::Joint => 1u8.hash(h),
+            }
+        }
+        TokenNode::Literal(ref lit) => (2u8, lit.to_string()).hash(h),
+        TokenNode::Term(ref word) => (3u8, word.as_str()).hash(h),
+    }
+}
+
+impl<'a> Hash for Tokens {
+    fn hash<H: Hasher>(&self, h: &mut H) {
+        self.tts.len().hash(h);
+        for tt in &self.tts {
+            tt_hash(&tt, h);
+        }
     }
 }
