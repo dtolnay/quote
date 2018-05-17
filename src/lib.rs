ab935db3f8fce8b0c3f778fe6a4d92835a510931
David Tolnay <dtolnay@gmail.com>
2018-05-17 10:45:45 -0700

    Remove references to quote/proc-macro feature

--- src/lib.rs
+++ src/lib.rs
@@ -94,8 +94,6 @@
 // Quote types in rustdoc of other crates get linked to here.
 #![doc(html_root_url = "https://docs.rs/quote/0.5.2")]
 
-#[cfg(feature = "proc-macro")]
-extern crate proc_macro;
 extern crate proc_macro2;
 
 mod tokens;
@@ -163,9 +161,8 @@ pub mod __rt {
 /// # Example
 ///
 /// ```
-/// # #[cfg(feature = "proc-macro")]
+/// # #[cfg(any())]
 /// extern crate proc_macro;
-/// # #[cfg(not(feature = "proc-macro"))]
 /// # extern crate proc_macro2 as proc_macro;
 ///
 /// #[macro_use]
