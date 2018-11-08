4151ef6f9c3a8b84c22c39b3381640e85ef0c359
David Tolnay <dtolnay@gmail.com>
2018-11-08 13:09:34 -0800

    Support i128 and u128 to tokens

--- tests/test.rs
+++ tests/test.rs
@@ -135,6 +135,19 @@ fn test_integer() {
     assert_eq!(expected, tokens.to_string());
 }
 
+#[cfg(integer128)]
+#[test]
+fn test_integer128() {
+    let ii128 = -1i128;
+    let uu128 = 1u128;
+
+    let tokens = quote! {
+        #ii128 #uu128
+    };
+    let expected = "-1i128 1u128";
+    assert_eq!(expected, tokens.to_string());
+}
+
 #[test]
 fn test_floating() {
     let e32 = 2.345f32;
