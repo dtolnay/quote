4a597c9c83982726f64331c77d6685a95be1ff2d
David Tolnay <dtolnay@gmail.com>
2020-12-29 17:20:34 -0800

    Opt in to pedantic clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,11 @@
 #![forbid(unsafe_code)]
 // Quote types in rustdoc of other crates get linked to here.
 #![doc(html_root_url = "https://docs.rs/quote/1.0.8")]
+#![allow(
+    clippy::doc_markdown,
+    clippy::missing_errors_doc,
+    clippy::module_name_repetitions
+)]
 
 #[cfg(all(
     not(all(target_arch = "wasm32", target_os = "unknown")),
