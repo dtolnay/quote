580174640767bc171f73d73ae3b84fbf44083a4f
David Tolnay <dtolnay@gmail.com>
2022-06-19 19:14:08 -0700

    Release 1.0.20

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@
 //! ```
 
 // Quote types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/quote/1.0.19")]
+#![doc(html_root_url = "https://docs.rs/quote/1.0.20")]
 #![allow(
     clippy::doc_markdown,
     clippy::missing_errors_doc,
