dcb4b0ed900047dd6598d5363871fe49b4960e65
David Tolnay <dtolnay@gmail.com>
2023-02-27 00:27:41 -0700

    Revert "Ignore using destructuring assignment to wildcard pattern"
    This reverts commit 1cafe1cad247c860f2e81f7cedd01de394a9d5c4.

--- benches/timer.rs
+++ benches/timer.rs
@@ -7,11 +7,11 @@ const ITERATIONS: u32 = 1000;
 pub fn time<T>(name: &'static str, function: impl Fn() -> T) {
     let begin = Instant::now();
     for _ in 0..ITERATIONS {
-        _ = function();
+        let _ = function();
     }
     let micros = (begin.elapsed() / ITERATIONS).as_micros();
     let mode = ["release", "debug"][cfg!(debug_assertions) as usize];
     let mut writer = StandardStream::stderr(ColorChoice::Auto);
-    _ = writer.set_color(ColorSpec::new().set_fg(Some(Color::Magenta)));
-    _ = writeln!(&mut writer, "{} in {} mode: {} micros", name, mode, micros);
+    let _ = writer.set_color(ColorSpec::new().set_fg(Some(Color::Magenta)));
+    let _ = writeln!(&mut writer, "{} in {} mode: {} micros", name, mode, micros);
 }
