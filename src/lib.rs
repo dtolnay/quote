d8cb63f7d7f45c503ac580bd8f3cb2d8bb28b160
David Tolnay <dtolnay@gmail.com>
2023-07-23 05:30:15 -0700

    Release 1.0.32

--- src/lib.rs
+++ src/lib.rs
@@ -92,7 +92,7 @@
 //! [prettyplease]: https://github.com/dtolnay/prettyplease
 
 // Quote types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/quote/1.0.31")]
+#![doc(html_root_url = "https://docs.rs/quote/1.0.32")]
 #![allow(
     clippy::doc_markdown,
     clippy::missing_errors_doc,
